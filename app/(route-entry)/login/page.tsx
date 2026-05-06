abd0d8360bd5ad2ed3580db05b2c1f18a575cde3
안예지 <ahnyejizzang@gmail.com>
2026-05-06 16:49:27 +0900

    refactor: 라우트 엔트리 구조 정리 및 로그인/회원가입 페이지 구현 분리

---
+++ app/(route-entry)/login/page.tsx
@@ -0,0 +1,4 @@
+/**
+ * @description /login 라우트 엔트리
+ */
+export { default } from "@/app/components/auth/login-page";
---
+++ app/(route-entry)/signup/page.tsx
@@ -0,0 +1,4 @@
+/**
+ * @description /signup 라우트 엔트리
+ */
+export { default } from "@/app/components/auth/signup-page";
